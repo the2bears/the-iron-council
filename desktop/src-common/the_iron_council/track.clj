b1902d7c00311da5574b2c526c771868bf85a826
William Swaney <the2bears@gmail.com>
2017-08-25 23:07:26 -0700

    Can thread 'create-track-sequence' together to easily create long tracks

--- desktop/src-common/the_iron_council/track.clj
+++ desktop/src-common/the_iron_council/track.clj
@@ -9,6 +9,7 @@
 (def track-width 32)
 (def track-height 4)
 (def track-tie-color (color 0.14 0.12 0.11 1))
+(def track-rail-color (color 0.42 0.44 0.47 1))
 
 (defn- draw-rects [pix-map c x y w h]
   (doto pix-map
@@ -16,13 +17,16 @@
     (pixmap! :fill-rectangle x y w h)))
 
 (defn- create-track-sequence
-  ([{:keys [x y v]} r n]
-   (create-track-sequence x y v r n))
+  ([s r n]
+   (let [{:keys [x y v]} (last s)]
+     (create-track-sequence x y v r s n)))
   ([x y v r n]
+   (create-track-sequence x y v r [] n))
+  ([x y v r acc n]
    (loop [x x
           y y
           c n
-          acc []
+          acc acc
           v v]
      (if (= c 0)
        acc
@@ -39,9 +43,11 @@
                 new-v))))))
                      
 (defn- create-track-texture []
-  (let [pix-map (pixmap* track-width track-height (pixmap-format :r-g-b-a8888))]
-    (draw-rects pix-map track-tie-color 0 0 track-width track-height)
-    (texture pix-map :set-region 0 0 track-width track-height)))
+  (let [pix-map (pixmap* track-width 16 (pixmap-format :r-g-b-a8888))]
+    (draw-rects pix-map track-tie-color 0 5 track-width track-height)
+    (draw-rects pix-map track-rail-color track-height 0 2 14)
+    (draw-rects pix-map track-rail-color 26 0 2 14)
+    (texture pix-map :set-region 0 0 track-width 14)))
 
 (defn create-track-entity
   [x y a]
@@ -51,7 +57,7 @@
                               @track-texture)
                             :else @track-texture)]
     (assoc track-texture
-           :width (c/screen-to-world 32) :height (c/screen-to-world 4)
+           :width (c/screen-to-world 32) :height (c/screen-to-world 14)
            :x x :y y :angle a
            :translate-x (c/screen-to-world (- (/ track-width 2)))
            :translate-y (c/screen-to-world (- (/ track-height 2)))
@@ -60,17 +66,16 @@
            :speed track-speed)))
 
 (defn create-curved-track []
-  (let [track-coords (create-track-sequence (/ c/game-width 2) 0 (vector-2 0 12) -3 10)
-        t-coords-s (create-track-sequence (last track-coords) 0 5)
-        t-coords-2 (create-track-sequence (last t-coords-s) 3 10)
-        snd-track (create-track-sequence (/ c/game-width 2) 0 (vector-2 0 12) 0 25)]
-    
+  (let [track (-> (create-track-sequence (/ c/game-width 2) 0 (vector-2 0 12) -3 10)
+                  (create-track-sequence 0 5)
+                  (create-track-sequence 3 10))]
+                  ;(create-track-sequence (/ c/game-width 2) 0 (vector-2 0 12) 0 25))]
     (reduce (fn [acc t](conj acc
                              (create-track-entity (c/screen-to-world (:x t))
                                                   (c/screen-to-world (:y t))
                                                   (- (:a t) 90))))
             []
-            (concat track-coords t-coords-s t-coords-2 snd-track))))
+            track))) ;(concat track-coords t-coords-s t-coords-2 snd-track))))
 (defn update-track
   [screen {:keys [y speed] :as entity}]
   (assoc entity :y (+ y speed)))
