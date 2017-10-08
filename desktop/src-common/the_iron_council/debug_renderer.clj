d2652f6b278432e1fcb3deb8d75abab64aff145e
William Swaney <the2bears@gmail.com>
2017-10-08 16:25:48 -0700

    Debug rendering for 'way-points' on train cars

--- desktop/src-common/the_iron_council/debug_renderer.clj
+++ desktop/src-common/the_iron_council/debug_renderer.clj
@@ -1,6 +1,7 @@
 (ns the-iron-council.debug-renderer
-  (:require [play-clj.core :refer :all]
-            [play-clj.math :refer :all])
+  (:require [play-clj.core :refer :all :as core]
+            [play-clj.math :refer :all]
+            [the-iron-council.common :refer [screen-to-world] :as c])
   (:import [com.badlogic.gdx.graphics.glutils ShapeRenderer]))
 
 (defn- render-collider [^ShapeRenderer shape-renderer
@@ -40,3 +41,28 @@
             (render-collider shape-renderer collidable))
           collidables))    
   entities)
+
+(defn render-test-bundle [^ShapeRenderer shape-renderer
+                          {:keys [way-points x y angle] :as test-bundle}]
+  (doto shape-renderer
+    (.setColor 0 1 0 1)
+    (.begin))
+  (run! (fn [[dx dy]]
+          (let [v (vector-2! (vector-2 dx dy) :rotate angle)]
+            (.circle shape-renderer (+ x (core/x v)) (+ y (core/y v)) 0.05 8))) ;(c/screen-to-world 1)))
+        way-points)
+  (.end shape-renderer))
+
+(defn render-way-points [{:keys [camera shape-renderer] :as screen} entities]
+  (doto shape-renderer
+    (.setProjectionMatrix (.combined camera))
+    (.setAutoShapeType true))  
+  (if-let [test-bundles (filter #(some? (:test-bundle? %)) entities)]
+    (run! (fn[test-bundle]
+            (render-test-bundle shape-renderer test-bundle))
+          test-bundles))
+  entities)
+
+
+
+
