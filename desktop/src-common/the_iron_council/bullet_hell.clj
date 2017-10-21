bc920f9813e0fdaeef8a14862a0da3407f6147cd
William Swaney <the2bears@gmail.com>
2017-10-21 09:53:33 -0700

    Improved doc strings for the functions.

--- desktop/src-common/the_iron_council/bullet_hell.clj
+++ desktop/src-common/the_iron_council/bullet_hell.clj
@@ -8,6 +8,9 @@
   (< (dec min-ticks) ticks max-ticks))
 
 (defn linear-movement
+  "Simple linear movement, from min-ticks (default 0) to max-ticks (default Integer/MAX_VALUE).
+   Each tick dx and dy ('d'eltas) are added to the bullet's x and y respectively. dx and dy are
+   added to the bullet's map."
   [& {:keys [dx dy min-ticks max-ticks]
       :or {dx 0.0 dy 0.0 min-ticks 0 max-ticks Integer/MAX_VALUE}}]
   (fn [{:keys [x y ticks] :or {ticks 0} :as bullet}]
@@ -20,6 +23,9 @@
              :ticks (inc ticks)))))
 
 (defn continue
+  "Continues in linear movement, from min-ticks (default 0) to max-ticks (default Integer/MAX_VALUE).
+   Each tick dx and dy ('d'eltas) are added to the bullet's x and y respectively. dx and dy are
+   obtained from the bullet's map, so they must be present already."
   [& {:keys [min-ticks max-ticks]
       :or {min-ticks 0 max-ticks Integer/MAX_VALUE}}]
   (fn [{:keys [x y dx dy ticks] :or {ticks 0} :as bullet}]
@@ -30,6 +36,10 @@
              :ticks (inc ticks)))))
 
 (defn accel
+  "Simple acceleration, from min-ticks (default 0) to max-ticks (default Integer/MAX_VALUE).
+   Each tick ax and ay ('a'cceleration) are added to dx and dy ('d'eltas) respectively.
+   Subsequently dx and dy are added to the bullet's x and y. dx and dy are added to the bullet's
+   map."
   [& {:keys [ax ay min-ticks max-ticks]
       :or {min-ticks 0 max-ticks Integer/MAX_VALUE}}]
   (fn [{:keys [x y dx dy ticks] :or {dx 0 dy 0 ticks 0} :as bullet}]
@@ -44,6 +54,9 @@
                :ticks (inc ticks))))))
 
 (defn rotate
+  "Simple rotation, from min-ticks (default 0) to max-ticks (default Integer/MAX_VALUE).
+   Each tick the vector of (dx, dy) is rotated by angle da. dx and dy are subsequently added to the
+   bullet's x and y respectively, and the bullet's map is updated with the new dx and dy values."
   [& {:keys [da min-ticks max-ticks]
       :or {min-ticks 0 max-ticks Integer/MAX_VALUE}}]
   (let [term (- max-ticks min-ticks)]
@@ -60,7 +73,10 @@
                  :ticks (inc ticks)))))))
 
 (defn change-direction
-  "Changes the bullets direction, to the target angle, in n terms. Up is 90, right is 0."
+  "Changes the bullets direction, to the target angle, in n terms (from min-ticks to max-ticks).
+   Up is 90, right is 0. sx and sy represent the starting velocity and are used to calculate the
+   starting angle of the bullet. Each tick then sees the dx and dy values recalculated by rotating
+   the vector. These values are updated in the bullet's map."
   [& {:keys [sx sy ta min-ticks max-ticks]
       :or {min-ticks 0 max-ticks Integer/MAX_VALUE}}]
   (let [term (- max-ticks min-ticks)
@@ -79,6 +95,10 @@
                  :ticks (inc ticks)))))))
 
 (defn change-speed
+  "Simple change in velocity, from min-ticks (default 0) to max-ticks (default Integer/MAX_VALUE).
+   sx/sy represent the 's'tarting velocity, tx/ty the 't'arget velocity. ax and ay are calculated
+   and subsequently added to dx and dy each tick. These are added the the bullet's x and y respectively
+   and updated in the bullet's map."
   [& {:keys [sx sy tx ty min-ticks max-ticks]
       :or {sx 0.0 sy 0.0 tx 0.0 ty 0.0 min-ticks 0 max-ticks Integer/MAX_VALUE}}]
   (let [term (- max-ticks min-ticks)
